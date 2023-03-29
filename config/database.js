0404fb5ff8d2b0659d8ad3577b7371b565b4db60
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-03-30 02:40:29 +0300

    database.js modified

--- config/database.js
+++ config/database.js
@@ -1,27 +1,28 @@
 module.exports = ({ env }) => ({
-    connection: {
-       client: 'postgres',
-       connection: {
-          host: env("DATABASE_HOST"),
-          port: env.int("DATABASE_PORT"),
-          database: env("DATABASE_NAME"),
-          user: env("DATABASE_USERNAME"),
-          password: env("DATABASE_PASSWORD"),
-          ssl: env.bool("DATABASE_SSL", true),
-    },
-    debug: false,
-    acquireConnectionTimeout: 5000,
-    pool: {
-      min: 0,
-      max: 10,
-      createTimeoutMillis: 8000,
-      acquireTimeoutMillis: 8000,
-      idleTimeoutMillis: 8000,
-      reapIntervalMillis: 1000,
-      createRetryIntervalMillis: 100
+   connection: {
+     client: 'postgres',
+     connection: {
+       host: env("DATABASE_HOST"),
+       port: env.int("DATABASE_PORT"),
+       database: env("DATABASE_NAME"),
+       user: env("DATABASE_USERNAME"),
+       password: env("DATABASE_PASSWORD"),
+       ssl: env.bool("DATABASE_SSL", true),
+     },
+     debug: false,
+     acquireConnectionTimeout: 10000,
+     pool: {
+       min: 2,
+       max: 20,
+       createTimeoutMillis: 10000, 
+       acquireTimeoutMillis: 10000, 
+       idleTimeoutMillis: 60000, 
+       reapIntervalMillis: 1000,
+       createRetryIntervalMillis: 100
+     }
    }
-}
-});
+ });
+ 
 
 
   
